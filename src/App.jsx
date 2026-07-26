5a13e7bdc577f0cf9955bbfa7eb8f01c880b58a3
valtraas <neptscode@gmail.com>
2026-07-26 19:43:17 +0700

    refactor: resize image to improve load

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Home from './sections/Home';
 import Service from './sections/Service';
 import AOS from "aos";
 import "aos/dist/aos.css";
+import logo from './assets/images/logo.webp';
+
 export default function App() {
   useEffect(()=> {
     AOS.init({
@@ -15,7 +17,10 @@ export default function App() {
   }, [])
   return (
     <div className='p-10'>
+    <div className="flex items-center gap-5">
+      <img src={logo} className="w-10" />
       <p className="uppercase font-bold text-secondary m:text-5xl font-display text-3xl">paradise<span className="text-secondary">.</span></p>
+    </div>
       <Hero />
       <Navbar />
       <Home />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,34 @@
 import { CiLocationOn } from "react-icons/ci";
 import { CiPhone } from "react-icons/ci";
+import logo from '../assets/images/logo.webp';
+
 export default function Footer() {
     return (
         <>
-        <section className="border-t-8 border-primary shadow-md p-6 md:flex items-center justify-between mt-20 mb-10 rounded-xl" id="kontak" data-aos="fade-up">
-            <div>
-                <p className="uppercase text-primary font-display text-4xl font-semibold">paradise<span className="text-secondary">.</span></p>
+            <section className="border-t-8 border-primary shadow-md p-6 md:flex items-center justify-between mt-20 mb-10 rounded-xl" id="kontak" data-aos="fade-up">
+                <div>
+                    <div className="flex items-center gap-5">
+                        <img src={logo} className="w-12" />
+                        <p className="uppercase text-primary font-display text-4xl font-semibold">paradise<span className="text-secondary">.</span></p>
+                    </div>
                     <p className="font-body text-body md:text-xl mt-3">Kunjungi kami langsung atau hubungi kami via WhatsApp untuk reservasi slot waktu favorit Anda sekarang!</p>
-                <div className="flex flex-col gap-5 mt-10 font-body">
-                    <div className="flex item-center gap-2 ">
-                        <CiLocationOn className="text-3xl  text-primary" />
-                        <p className="md:text-xl text-body">Jl. Prawirotaman, Brontokusuman</p>
-                    </div>
-                    <div className="flex item-center gap-2">
-                        <CiPhone className="text-3xl  text-primary" />
-                        <p className="md:text-xl text-body">+62 8571-9290-327</p>
-                    </div>
-                  
-                </div>
-            </div>
-            <div className="h-72 rounded-xl">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.7249128236986!2d110.3691951!3d-7.8189148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59005364a5b9%3A0x7b2e1db1d5234beb!2sParadise%20Massage!5e0!3m2!1sid!2sid!4v1784875186309!5m2!1sid!2sid"  loading="lazy" className="w-full h-full rounded-xl md:my-0 my-10"></iframe>
+                    <div className="flex flex-col gap-5 mt-10 font-body">
+                        <div className="flex item-center gap-2 ">
+                            <CiLocationOn className="text-3xl  text-primary" />
+                            <p className="md:text-xl text-body">Jl. Prawirotaman, Brontokusuman</p>
+                        </div>
+                        <div className="flex item-center gap-2">
+                            <CiPhone className="text-3xl  text-primary" />
+                            <p className="md:text-xl text-body">+62 8571-9290-327</p>
+                        </div>
 
-            </div>
-        </section>
+                    </div>
+                </div>
+                <div className="h-72 rounded-xl">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.7249128236986!2d110.3691951!3d-7.8189148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59005364a5b9%3A0x7b2e1db1d5234beb!2sParadise%20Massage!5e0!3m2!1sid!2sid!4v1784875186309!5m2!1sid!2sid" loading="lazy" className="w-full h-full rounded-xl md:my-0 my-10"></iframe>
+
+                </div>
+            </section>
             <p className=" text-body font-display font-semibold text-center">
                 © 2026 Paradise Massage & Treatment. All Rights Reserved.
             </p>
